44300f65f2777d462fc09da00eb49633902a31c4
abzink <s213027@student.dtu.dk>
2023-01-04 22:20:52 +0100

    debugging

--- s4_debugging_and_logging/exercise_files/vae_mnist_bugs.py
+++ s4_debugging_and_logging/exercise_files/vae_mnist_bugs.py
@@ -13,7 +13,7 @@ from torchvision.utils import save_image
 
 # Model Hyperparameters
 dataset_path = 'datasets'
-cuda = True
+cuda = torch.cuda.is_available()
 DEVICE = torch.device("cuda" if cuda else "cpu")
 batch_size = 100
 x_dim  = 784
@@ -61,7 +61,7 @@ class Decoder(nn.Module):
     def __init__(self, latent_dim, hidden_dim, output_dim):
         super(Decoder, self).__init__()
         self.FC_hidden = nn.Linear(latent_dim, hidden_dim)
-        self.FC_output = nn.Linear(latent_dim, output_dim)
+        self.FC_output = nn.Linear(hidden_dim, output_dim)
         
     def forward(self, x):
         h     = torch.relu(self.FC_hidden(x))
